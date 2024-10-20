33ddff10adc26fa48f0fe046fb13e073bd569f6d
Fonij80 <forooz.iraji@gmail.com>
2024-10-20 20:29:22 +0330

    complete TodoAPI

--- Todo/todos/tests.py
+++ Todo/todos/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from .models import Todo
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class TodoModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.todo = Todo.objects.create(
+            title = "Title1",
+            description = "First Todo",
+        )
+
+    def test_model_content(self):
+        self.assertEqual(self.todo.title, "Title1")
+        self.assertEqual(self.todo.description, "First Todo")
+        self.assertEqual(str(self.todo), "Title1")
+
+    def test_api_listview(self):  # new
+        response = self.client.get(reverse("todo_list"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Todo.objects.count(), 1)
+        self.assertContains(response, self.todo)
+
+    def test_api_detailview(self):  # new
+        response = self.client.get(
+            reverse("todo_detail", kwargs={"pk": self.todo.id}),
+            format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Todo.objects.count(), 1)
+        self.assertContains(response, "First Todo")
